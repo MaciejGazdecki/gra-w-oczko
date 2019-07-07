4630ce19a0a75f2f4324515e9eb9b9528233dd76
Maciej Gazdecki <maciej.gazdecki@gmail.com>
2019-07-07 19:02:23 +0200

    I rebuilt the code to addapt it for two player version

--- js/main.js
+++ js/main.js
@@ -1,24 +1,39 @@
 (function init() {
-    // all results and game control variables
-    const gameResult = document.querySelector('#result');
-    const allResults = document.querySelector('#not_wins');
-    const twentyOnes = document.querySelector('#twenty_ones');
-    const twoAces = document.querySelector('#two_aces');
+    // main variables
     const mainGameCounter = document.querySelector('#game_counter');
+    let gameCounter = 0;
+    let gamePlaying;
+    let activePlayer = 0;
+
+    //player 1 query selectors
+    const gameResult1 = document.querySelector('#result-0');
+    const wins1 = document.querySelector('#wins-0');
+    const twentyOnes1 = document.querySelector('#twenty_ones-0');
+    const twoAces1 = document.querySelector('#two_aces-0');
+    const lost1 = document.querySelector('#games_lost-0');
+
+    //player 2 query selectors
+    const gameResult2 = document.querySelector('#result-1');
+    const wins2 = document.querySelector('#wins-2');
+    const twentyOnes2 = document.querySelector('#twenty_ones-1');
+    const twoAces2 = document.querySelector('#two_aces-1');
+    const lost2 = document.querySelector('#games_lost-1');
+
+    //results arrays;
+    let notWins = [0,0];
+    let twentyOnesResults = [0,0];
+    let twoAcesResults = [0,0];
+    let gameLostResults = [0,0];
+    let actualResult = [0,0];
+
+    //popups
     const youLostPopup = document.querySelector('.you_lost');
     const congratulationsPopup = document.querySelector('.congratulations');
     const twoAcesPopup = document.querySelector('.congratulations_two_aces');
-    let notWins = 0;
-    let twentyOnesResults = 0;
-    let twoAcesResults = 0;
-    let gameLostResults = 0;
-    let actualResult = 0;
-    let gameCounter = 0;
-    let gamePlaying;
-
 
     // cards related things
-    const allCards = document.querySelectorAll('.card');
+    const cards_1 = document.querySelectorAll('.card-0');
+    const cards_2 = document.querySelectorAll('.card-1');
     const cards = ['2C', '2D', '2H', '2S', '3C', '3D', '3H', '3S', '4C', '4D', '4H', '4S', '5C', '5D', '5H', '5S', '6C', '6D', '6H', '6S', '7C', '7D', '7H', '7S', '8C', '8D', '8H', '8S', '9C', '9D', '9H', '9S', '10C', '10D', '10H', '10S', '2JC', '2JD', '2JH', '2JS', '3QC', '3QD', '3QH', '3QS', '4KC', '4KD', '4KH', '4KS', '11AC', '11AD', '11AH', '11AS'];
     let cardCounter = 0;
 
@@ -30,43 +45,55 @@
             }
         }
         shuffle(cards);
-        gameResult.textContent = 0;
-        allCards.forEach(card => card.setAttribute('src', 'images/gray_back.png'));
+        gameResult1.textContent = 0;
+        cards_1.forEach(card => card.setAttribute('src', 'images/gray_back.png'));
+        cards_2.forEach(card => card.setAttribute('src', 'images/yellow_back.png'));
         youLostPopup.style.display = 'none';
         congratulationsPopup.style.display = 'none';
         twoAcesPopup.style.display = 'none';
-        if(actualResult >= 22 && cardCounter > 2) {
-            gameLostResults++;
-            document.querySelector('#games_lost').textContent = gameLostResults;
-        }
+        // if(actualResult >= 22 && cardCounter > 2) {
+        //     gameLostResults++;
+        //     document.querySelector('#games_lost').textContent = gameLostResults;
+        // }
         cardCounter = 0;
-        if (actualResult > 0) {
-            gameCounter++;
-            mainGameCounter.textContent = gameCounter;
-        }
-        showPercentages();
-        actualResult = 0;
+        // if (actualResult > 0) {
+        //     gameCounter++;
+        //     mainGameCounter.textContent = gameCounter;
+        // }
+        actualResult = [0,0];
         gamePlaying = true;
+        document.querySelector(`#card-0-${cardCounter}`).src = `images/${cards[cardCounter]}.png`;
+        document.querySelector(`#card-1-${cardCounter+1}`).src = `images/${cards[cardCounter+1]}.png`;
+        actualResult[activePlayer] = parseInt(cards[cardCounter]);
+        actualResult[activePlayer +1] = parseInt(cards[cardCounter+1]);
+        gameResult1.textContent = actualResult[activePlayer];
+        gameResult2.textContent = actualResult[activePlayer +1];
+        cardCounter = 2;
+        activePlayer = 0;
     }
 
     //take card function
     function takeCard() {
         if (gamePlaying) {
-            if (actualResult < 21) {
-                document.querySelector(`#card-${cardCounter + 1}`).src = `images/${cards[cardCounter]}.png`;
-                actualResult += parseInt(cards[cardCounter]);
-                gameResult.textContent = actualResult;
+            if (actualResult[activePlayer] < 21) {
+                document.querySelector(`#card-${activePlayer}-${cardCounter}`).src = `images/${cards[cardCounter]}.png`;
+                actualResult[activePlayer] += parseInt(cards[cardCounter]);
+                document.querySelector(`#result-${activePlayer}`).textContent = actualResult[activePlayer];
                 cardCounter++;
-            } else if (actualResult === 21) {
+                activePlayer++;
+                if(activePlayer === 2){
+                    activePlayer = 0;
+                }
+            } else if (actualResult[activePlayer] === 21) {
                 gamePlaying = false;
-                twentyOnesResults++;
-                twentyOnes.textContent = twentyOnesResults;
+                twentyOnesResults[activePlayer]++;
+                document.querySelector(`#twenty_ones-${activePlayer}`).textContent = twentyOnesResults[activePlayer];
                 congratulationsPopup.style.display = 'flex';
-            } else if (actualResult === 22 && cardCounter === 2) {//when the actualesult is 22 and cardCounter is 2 it means that
+            } else if (actualResult[activePlayer] === 22 && cardCounter === 2) {//when the actualesult is 22 and cardCounter is 2 it means that
                 //there are two cards only  with value 11 it must be aces
                 gamePlaying = false;
-                twoAcesResults++;
-                twoAces.textContent = twoAcesResults;
+                twoAcesResults[activePlayer]++;
+                document.querySelector(`#two_aces-${activePlayer}`).textContent = twoAcesResults[activePlayer];
                 twoAcesPopup.style.display = 'flex';
             } else {
                 gamePlaying = false;
@@ -77,18 +104,18 @@
 
     //take point function
     function takePoints() {
-        if (actualResult === 0) {
+        if (actualResult[activePlayer] === 0 && cardCounter === 0) {
+            gamePlaying = false;
+        } else if (actualResult[activePlayer] > 0 && actualResult[activePlayer] < 21) {
+            
+
             startGame();
-        } else if (actualResult > 0 && actualResult < 21) {
-            notWins++;
-            allResults.textContent = notWins;
-            startGame();
-        } else if (actualResult === 21) {
+        } else if (actualResult[activePlayer] === 21) {
             gamePlaying = false;
             twentyOnesResults++;
             twentyOnes.textContent = twentyOnesResults;
             congratulationsPopup.style.display = 'flex'
-        } else if (actualResult === 22 && cardCounter === 2) {
+        } else if (actualResult[activePlayer] === 22 && cardCounter === 2) {
             gamePlaying = false;
             twoAcesResults++;
             twoAces.textContent = twoAcesResults;
@@ -98,17 +125,7 @@
             youLostPopup.style.display = 'flex';
         }
     }
-
-    //percentage show
-    function showPercentages () {
-        if (gameCounter > 0) {
-            document.querySelector('#notwins').textContent = `${((notWins / gameCounter) * 100).toFixed(2)}%`;
-            document.querySelector('#twentyones').textContent = `${((twentyOnesResults / gameCounter) * 100).toFixed(2)}%`;
-            document.querySelector('#twoaces').textContent = `${((twoAcesResults / gameCounter) * 100).toFixed(2)}%`;
-            document.querySelector('#lost').textContent = `${((gameLostResults / gameCounter) * 100).toFixed(2)}%`;
-        }
-    }
-
+    
     //event listeners
     document.querySelectorAll('.start').forEach(btn => btn.addEventListener('click', startGame));
     document.querySelector('.take_card').addEventListener('click', takeCard);
