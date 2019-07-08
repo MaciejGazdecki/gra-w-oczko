387b719bfecd0142adbf71f379e9f3ccc1cebce5
GAZDECKI <Maciej.GAZDECKI@newellco.com>
2019-07-08 17:01:20 +0200

    improvements

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 (function init() {
     // main variables
-    const mainGameCounter = document.querySelector('#game_counter');
     let gameCounter = 0;
+    document.querySelector('#game_counter').textContent = gameCounter;
+    let drawsCounter = 0;
     let gamePlaying;
     let activePlayer = 0;
     let playerSwing = [true, true];
-    let helperCardCounter = 1;
+
+    //results arrays;
+    let actualResult = [0, 0];
+    let wins = [0, 0];
+    let twentyOnesResults = [0, 0];
+    let twoAcesResults = [0, 0];
+    let gameLostResults = [0, 0];
 
     //player 0 query selectors
     const gameResult0 = document.querySelector('#result-0');
@@ -16,22 +23,16 @@
 
     //player 1 query selectors
     const gameResult1 = document.querySelector('#result-1');
-    const wins1 = document.querySelector('#wins-2');
+    const wins1 = document.querySelector('#wins-1');
     const twentyOne1 = document.querySelector('#twenty_ones-1');
     const twoAces1 = document.querySelector('#two_aces-1');
     const lost1 = document.querySelector('#games_lost-1');
-
-    //results arrays;
-    let actualResult = [0,0];
-    let wins = [0,0];
-    let twentyOnesResults = [0,0];
-    let twoAcesResults = [0,0];
-    let gameLostResults = [0,0];
 
     //popups
     const youLostPopup = document.querySelector('.you_lost');
     const congratulationsPopup = document.querySelector('.congratulations');
     const twoAcesPopup = document.querySelector('.congratulations_two_aces');
+    const whoWinsPopup = document.querySelector('.who-wins');
 
     // cards related things
     const cards_1 = document.querySelectorAll('.card-0');
@@ -40,6 +41,7 @@
         '6H', '6S', '7C', '7D', '7H', '7S', '8C', '8D', '8H', '8S', '9C', '9D', '9H', '9S', '10C', '10D', '10H', '10S', '2JC',
         '2JD', '2JH', '2JS', '3QC', '3QD', '3QH', '3QS', '4KC', '4KD', '4KH', '4KS', '11AC', '11AD', '11AH', '11AS'];
     let cardCounter = 0;
+    let helperCardCounter = 1;
 
     //start game function
     function startGame() {
@@ -54,6 +56,7 @@
         youLostPopup.style.display = 'none';
         congratulationsPopup.style.display = 'none';
         twoAcesPopup.style.display = 'none';
+        whoWinsPopup.style.display = 'none'
         // if(actualResult >= 22 && cardCounter > 2) {
         //     gameLostResults++;
         //     document.querySelector('#games_lost').textContent = gameLostResults;
@@ -65,7 +68,7 @@
         //     gameCounter++;
         //     mainGameCounter.textContent = gameCounter;
         // }
-        actualResult = [0,0];
+        actualResult = [0, 0];
         gamePlaying = true;
         playerSwing = [true, true];
         document.querySelector(`#card-0-${cardCounter}`).src = `images/${cards[cardCounter]}.png`;
@@ -76,14 +79,13 @@
         cardCounter++;
         gameResult0.textContent = actualResult[0];
         gameResult1.textContent = actualResult[1];
-        console.log(cards);
     }
 
     //take card function
     function takeCard() {
         if (gamePlaying) {
             if (actualResult[activePlayer] < 21) {
-                if(playerSwing[0] && playerSwing[1]) {
+                if (playerSwing[0] && playerSwing[1]) {
                     document.querySelector(`#card-${activePlayer}-${cardCounter}`).src
                         = `images/${cards[cardCounter]}.png`;
                     actualResult[activePlayer] += parseInt(cards[cardCounter]);
@@ -91,13 +93,16 @@
                     cardCounter++;
                     activePlayer++;
                     if (activePlayer === 2) activePlayer = 0;
-                } else {
-                    document.querySelector(`#card-${activePlayer}-${cardCounter+helperCardCounter}`).src
+                } else if ((!playerSwing[0] && playerSwing[1]) || (playerSwing[0] && !playerSwing[1])) {
+                    document.querySelector(`#card-${activePlayer}-${cardCounter + helperCardCounter}`).src
                         = `images/${cards[cardCounter]}.png`;
                     actualResult[activePlayer] += parseInt(cards[cardCounter]);
                     document.querySelector(`#result-${activePlayer}`).textContent = actualResult[activePlayer];
                     cardCounter++;
                     helperCardCounter++;
+                } else {
+                    gamePlaying = false;
+                    determineWinner(actualResult[0], actualResult[1])
                 }
             } else if (actualResult[activePlayer] === 21) {
                 gamePlaying = false;
@@ -124,7 +129,7 @@
         } else if (actualResult[activePlayer] > 0 && actualResult[activePlayer] < 21) {
             playerSwing[activePlayer] = false;
             activePlayer++;
-            if(activePlayer === 2) activePlayer = 0;
+            if (activePlayer === 2) activePlayer = 0;
         } else if (actualResult[activePlayer] === 21) {
             gamePlaying = false;
             twentyOnesResults++;
@@ -140,7 +145,28 @@
             youLostPopup.style.display = 'flex';
         }
     }
-    
+
+    //winner determining
+    function determineWinner(player0, player1) {
+        if (player0 > player1) {
+            wins[0]++;
+            wins0.textContent = wins[0];
+            gameLostResults[1]++;
+            lost1.textContent = gameLostResults[1];
+            document.querySelector('#winner').textContent = '1';
+            whoWinsPopup.style.display = 'flex';
+        } else if (player0 < player1) {
+            wins[1]++;
+            wins1.textContent = wins[1];
+            document.querySelector('#winner').textContent = '2';
+            whoWinsPopup.style.display = 'flex';
+        } else {
+            drawsCounter++;
+            document.querySelector('#draw').textContent = drawsCounter;
+            document.querySelector('.who-wins p').textContent = 'REMIS!!!!'
+        }
+    }
+
     //event listeners
     document.querySelectorAll('.start').forEach(btn => btn.addEventListener('click', startGame));
     document.querySelector('.take_card').addEventListener('click', takeCard);
