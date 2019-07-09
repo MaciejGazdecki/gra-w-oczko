94df279f5b21693df7bc60e2e4471f4f297d1ccc
Maciej Gazdecki <maciej.gazdecki@gmail.com>
2019-07-09 23:12:33 +0200

    bug fix

--- js/main.js
+++ js/main.js
@@ -34,7 +34,7 @@
     // cards related things
     const cards_1 = document.querySelectorAll('.card-0');
     const cards_2 = document.querySelectorAll('.card-1');
-    const cards = ['2C', '2D', '2H', '2S', '3C', '3D', '3H', '3S', '4C', '4D', '4H', '4S', '5C', '5D', '5H', '5S', '6C', '6D',
+    const cards = ['11AC', '11AD', '11AH', '10S','2C', '2D', '2H', '2S', '3C', '3D', '3H', '3S', '4C', '4D', '4H', '4S', '5C', '5D', '5H', '5S', '6C', '6D',
         '6H', '6S', '7C', '7D', '7H', '7S', '8C', '8D', '8H', '8S', '9C', '9D', '9H', '9S', '10C', '10D', '10H', '10S', '2JC',
         '2JD', '2JH', '2JS', '3QC', '3QD', '3QH', '3QS', '4KC', '4KD', '4KH', '4KS', '11AC', '11AD', '11AH', '11AS'];
     let cardCounter = 0;
@@ -47,7 +47,7 @@
                 array.sort(() => Math.random() - 0.5);
             }
         }
-        shuffle(cards);
+        // shuffle(cards);
         cards_1.forEach(card => card.setAttribute('src', 'images/gray_back.png'));
         cards_2.forEach(card => card.setAttribute('src', 'images/yellow_back.png'));
         twoAcesPopup.style.display = 'none';
@@ -99,16 +99,11 @@
 
     //take point function
     function takePoints() {
-        if (actualResult[activePlayer] === 0 && cardCounter === 0) {
-            gamePlaying = false;
-        } else if (actualResult[activePlayer] > 0) {
-            playerSwing[activePlayer] = false;
-            activePlayer++;
-            if (activePlayer === 2) activePlayer = 0;
-            if (!playerSwing[0] && !playerSwing[1]) {
-                determineWinner(actualResult);
-                gamePlaying = false;
-            }
+        playerSwing[activePlayer] = false;
+        activePlayer++;
+        if (activePlayer === 2) activePlayer = 0;
+        if (!playerSwing[0] && !playerSwing[1]) {
+            determineWinner(actualResult);
         }
     }
 
@@ -135,27 +130,6 @@
                 document.querySelector('.who-wins p').textContent = 'REMIS!!!!';
                 whoWinsPopup.style.display = 'flex';
             }
-        } else if ((array[0] === 21 || array[1] === 21) || (array[0] === 21 && array[1] === 21)){
-            if(array[0] ===21 && array[1] === 21) {
-                drawsCounter++;
-                document.querySelector('#draw').textContent = drawsCounter;
-                document.querySelector('.who-wins p').textContent = 'ALE REMIS!!!!DWA OCZKA';
-                whoWinsPopup.style.display = 'flex';
-            } else if (array[0] === 21) {
-                twentyOnesResults[0]++;
-                twentyOne0.textContent = twentyOnesResults[0];
-                gameLostResults[1]++;
-                lost1.textContent = gameLostResults[1];
-                document.querySelector('.who-wins p').textContent = 'OCZKO DLA GRACZA 1, KTÓRY WYGRYWA!!';
-                whoWinsPopup.style.display = 'flex';
-            } else if (array[1] === 21) {
-                twentyOnesResults[1]++;
-                twentyOne1.textContent = twentyOnesResults[1];
-                gameLostResults[0]++;
-                lost1.textContent = gameLostResults[0];
-                document.querySelector('.who-wins p').textContent = 'OCZKO DLA GRACZA 2, KTÓRY WYGRYWA!!';
-                whoWinsPopup.style.display = 'flex';
-            }
         } else if (((array[0] === 22 || array[1] === 22) && cardCounter === 4) || (array[0] === 22 && array[1] === 22 
             && cardCounter === 4)) {
             if(array[0] ===22 && array[1] === 22) {
@@ -178,16 +152,37 @@
                 document.querySelector('.congratulations_two_aces p').textContent = ' PERSKIE OCZKO DLA GRACZA 2, KTÓRY WYGRYWA!!';
                 twoAcesPopup.style.display = 'flex';
             }
-        } else if (array[0] >= 22) {
+        } else if ((array[0] === 21 || array[1] === 21) || (array[0] === 21 && array[1] === 21)){
+            if(array[0] ===21 && array[1] === 21) {
+                drawsCounter++;
+                document.querySelector('#draw').textContent = drawsCounter;
+                document.querySelector('.who-wins p').textContent = 'ALE REMIS!!!!DWA OCZKA';
+                whoWinsPopup.style.display = 'flex';
+            } else if (array[0] === 21) {
+                twentyOnesResults[0]++;
+                twentyOne0.textContent = twentyOnesResults[0];
+                gameLostResults[1]++;
+                lost1.textContent = gameLostResults[1];
+                document.querySelector('.who-wins p').textContent = 'OCZKO DLA GRACZA 1, KTÓRY WYGRYWA!!';
+                whoWinsPopup.style.display = 'flex';
+            } else if (array[1] === 21) {
+                twentyOnesResults[1]++;
+                twentyOne1.textContent = twentyOnesResults[1];
+                gameLostResults[0]++;
+                lost1.textContent = gameLostResults[0];
+                document.querySelector('.who-wins p').textContent = 'OCZKO DLA GRACZA 2, KTÓRY WYGRYWA!!';
+                whoWinsPopup.style.display = 'flex';
+            }
+        } else if (array[0] >= 22 && cardCounter > 4) {
             wins[1]++;
             wins1.textContent = wins[1];
             gameLostResults[0]++;
             lost0.textContent = gameLostResults[0];
             document.querySelector('.who-wins p').textContent = 'GRACZ 2 WYGRYWA!!';
             whoWinsPopup.style.display = 'flex';
-        } else if (array[1] >= 22){
+        } else if (array[1] >= 22 && cardCounter > 4){
             wins[0]++;
-             wins0.textContent = wins[0];
+            wins0.textContent = wins[0];
             gameLostResults[1]++;
             lost1.textContent = gameLostResults[1];
             document.querySelector('.who-wins p').textContent = 'GRACZ 1 WYGRYWA!!';
