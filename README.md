b0a570c4b704017cd695c9b5b2341ecb5f500bed
Maciej Gazdecki <maciej.gazdecki@gmail.com>
2019-09-03 18:16:48 +0200

    changes

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # Polish version of Twenty-One game...(simplified :-)) in polish “Oczko”
 
-This is my first web game, written in JavaScript. After extensive learning of JS I wrote the polish version of BlackJack. This is one player version only without any RWD scaling, good to display on 16:9 screen the best on 1920*1080px. In the future I will prepare mobile version.  The game was developed as well and I created a two player version on another branch. The rules of that game are very easy.  The purpose of that game is to reach 21 points without without going over.  The only one exception is, when you obtain two Aces in the the first and second round, you win as well and it does not matter that you gained 22 points. The game is played with standard 52 card deck with Clubs, Diamonds, Hearts and Spades. The value of each card from 2-10 is the value of the card. The value of the Jack is 2 points, the value of the Queen is 3 points and the value of the King is 4 points. The Ace in this option has always the value of 11 points.
+This is my first web game, written in JavaScript. After extensive learning of JS I wrote the polish version of BlackJack. This is two player version only without any RWD scaling, good to display on 16:9 screen the best on 1920*1080px. In the future I will prepare mobile version. 
+
+The rules of that game are very easy.  The purpose of that game is to reach 21 points without without going over.  The only one exception is, when you obtain two Aces in the the first and second round, you win as well and it does not matter that you gained 22 points. The two aces always wins, even if the opponent reach 21 points in the first two round. It could happend that both players collect two aces. It is a very low probability that it will happend, but I programmed it as a draw.
+
+The game is played with standard 52 card deck with Clubs, Diamonds, Hearts and Spades. The value of each card from 2-10 is the value of the card. The value of the Jack is 2 points, the value of the Queen is 3 points and the value of the King is 4 points. The Ace in this option has always the value of 11 points.
 
 To play the game:
 
 1.Press Start (START GRY);<br>
 2.Start taking cards, by pressing Take Card (WEŹ KARTĘ);
 
-If you are close to 21 points or in any moment you can collect the pints or go ahead. Remember, that if you pass 21 points you loose. If you reach, exactly 21 points, you win and you need just press collect points. The final score of the each won game (without passing 21 points) will be recorded and displayed. I added as well an average score of each won game.
+If you are close to 21 points or in any moment you can collect the points and pass or go ahead. Remember, that if you go over 21 points you automatically loose. If you reach, exactly 21 points, you need to pass and you wait until your opponent pass. If your oponent reach as well 21 points it is a draw, if he goes over 21, you win. 
 
 I hope you like it :-)
--- js/main.js
+++ js/main.js
@@ -1,115 +1,198 @@
 (function init() {
-    // all results and game control variables
-    const gameResult = document.querySelector('#result');
-    const allResults = document.querySelector('#not_wins');
-    const twentyOnes = document.querySelector('#twenty_ones');
-    const twoAces = document.querySelector('#two_aces');
-    const mainGameCounter = document.querySelector('#game_counter');
-    const youLostPopup = document.querySelector('.you_lost');
-    const congratulationsPopup = document.querySelector('.congratulations');
-    const twoAcesPopup = document.querySelector('.congratulations_two_aces');
-    let notWins = 0;
-    let twentyOnesResults = 0;
-    let twoAcesResults = 0;
-    let gameLostResults = 0;
-    let actualResult = 0;
+    // main variables
     let gameCounter = 0;
+    let drawsCounter = 0;
     let gamePlaying;
+    let activePlayer = 0;
+    let playerSwing = [true, true];
 
+    //results arrays;
+    let actualResult = [0, 0];
+    let wins = [0, 0];
+    let twentyOnesResults = [0, 0];
+    let twoAcesResults = [0, 0];
+    let gameLostResults = [0, 0];
+
+    //player 0 query selectors
+    const gameResult0 = document.querySelector('#result-0');
+    const wins0 = document.querySelector('#wins-0');
+    const twentyOne0 = document.querySelector('#twenty_ones-0');
+    const twoAces0 = document.querySelector('#two_aces-0');
+    const lost0 = document.querySelector('#games_lost-0');
+
+    //player 1 query selectors
+    const gameResult1 = document.querySelector('#result-1');
+    const wins1 = document.querySelector('#wins-1');
+    const twentyOne1 = document.querySelector('#twenty_ones-1');
+    const twoAces1 = document.querySelector('#two_aces-1');
+    const lost1 = document.querySelector('#games_lost-1');
+
+    //popups
+    const whoWinsPopup = document.querySelector('.who-wins');
+    const twoAcesPopup = document.querySelector('.congratulations_two_aces');
 
     // cards related things
-    const allCards = document.querySelectorAll('.card');
-    const cards = ['2C', '2D', '2H', '2S', '3C', '3D', '3H', '3S', '4C', '4D', '4H', '4S', '5C', '5D', '5H', '5S', '6C', '6D', '6H', '6S', '7C', '7D', '7H', '7S', '8C', '8D', '8H', '8S', '9C', '9D', '9H', '9S', '10C', '10D', '10H', '10S', '2JC', '2JD', '2JH', '2JS', '3QC', '3QD', '3QH', '3QS', '4KC', '4KD', '4KH', '4KS', '11AC', '11AD', '11AH', '11AS'];
+    const cards_1 = document.querySelectorAll('.card-0');
+    const cards_2 = document.querySelectorAll('.card-1');
+    const cards = ['2C', '2D', '2H', '2S', '3C', '3D', '3H', '3S', '4C', '4D', '4H', '4S', '5C', '5D', '5H', '5S', '6C', '6D',
+        '6H', '6S', '7C', '7D', '7H', '7S', '8C', '8D', '8H', '8S', '9C', '9D', '9H', '9S', '10C', '10D', '10H', '10S', '2JC',
+        '2JD', '2JH', '2JS', '3QC', '3QD', '3QH', '3QS', '4KC', '4KD', '4KH', '4KS', '11AC', '11AD', '11AH', '11AS'];
     let cardCounter = 0;
+    let helperCardCounter = 1;
 
     //start game function
     function startGame() {
-        function shuffle(array) { //simple shuffling algorithm, is not ideal, but works
+        function shuffle(array) {
             for (let i = 0; i < 10000; i++) {
                 array.sort(() => Math.random() - 0.5);
             }
         }
         shuffle(cards);
-        gameResult.textContent = 0;
-        allCards.forEach(card => card.setAttribute('src', 'images/gray_back.png'));
-        youLostPopup.style.display = 'none';
-        congratulationsPopup.style.display = 'none';
+        cards_1.forEach(card => card.setAttribute('src', 'images/gray_back.png'));
+        cards_2.forEach(card => card.setAttribute('src', 'images/yellow_back.png'));
         twoAcesPopup.style.display = 'none';
-        if(actualResult >= 22 && cardCounter > 2) {
-            gameLostResults++;
-            document.querySelector('#games_lost').textContent = gameLostResults;
-        }
+        whoWinsPopup.style.display = 'none';
         cardCounter = 0;
-        if (actualResult > 0) {
-            gameCounter++;
-            mainGameCounter.textContent = gameCounter;
-        }
-        showPercentages();
-        actualResult = 0;
+        activePlayer = 0;
+        helperCardCounter = 1;
+        actualResult = [0, 0];
         gamePlaying = true;
+        playerSwing = [true, true];
+        document.querySelector(`#card-0-${cardCounter}`).src = `images/${cards[cardCounter]}.png`;
+        actualResult[0] = parseInt(cards[cardCounter]);
+        cardCounter++;
+        document.querySelector(`#card-1-${cardCounter}`).src = `images/${cards[cardCounter]}.png`;
+        actualResult[1] = parseInt(cards[cardCounter]);
+        cardCounter++;
+        gameResult0.textContent = actualResult[0];
+        gameResult1.textContent = actualResult[1];
     }
 
     //take card function
     function takeCard() {
         if (gamePlaying) {
-            if (actualResult < 21) {
-                document.querySelector(`#card-${cardCounter + 1}`).src = `images/${cards[cardCounter]}.png`;
-                actualResult += parseInt(cards[cardCounter]);
-                gameResult.textContent = actualResult;
-                cardCounter++;
-            } else if (actualResult === 21) {
-                gamePlaying = false;
-                twentyOnesResults++;
-                twentyOnes.textContent = twentyOnesResults;
-                congratulationsPopup.style.display = 'flex';
-            } else if (actualResult === 22 && cardCounter === 2) {//when the actualesult is 22 and cardCounter is 2 it means that
-                //there are two cards only  with value 11 it must be aces
-                gamePlaying = false;
-                twoAcesResults++;
-                twoAces.textContent = twoAcesResults;
-                twoAcesPopup.style.display = 'flex';
-            } else {
-                gamePlaying = false;
-                youLostPopup.style.display = 'flex';
+            if (actualResult[activePlayer] < 21) {
+                if (playerSwing[0] && playerSwing[1]) {
+                    document.querySelector(`#card-${activePlayer}-${cardCounter}`).src
+                        = `images/${cards[cardCounter]}.png`;
+                    actualResult[activePlayer] += parseInt(cards[cardCounter]);
+                    document.querySelector(`#result-${activePlayer}`).textContent = actualResult[activePlayer];
+                    cardCounter++;
+                    activePlayer++;
+                    if (activePlayer === 2) activePlayer = 0;
+                    if((actualResult[0] === 22 || actualResult[1] === 22) && cardCounter === 4) determineWinner(actualResult);
+                    if((actualResult[0] > 21 || actualResult[1] > 21)) determineWinner(actualResult);
+                } else if ((!playerSwing[0] && playerSwing[1]) || (playerSwing[0] && !playerSwing[1])) {
+                    document.querySelector(`#card-${activePlayer}-${cardCounter + helperCardCounter}`).src
+                        = `images/${cards[cardCounter]}.png`;
+                    actualResult[activePlayer] += parseInt(cards[cardCounter]);
+                    document.querySelector(`#result-${activePlayer}`).textContent = actualResult[activePlayer];
+                    cardCounter++;
+                    helperCardCounter++;
+                    if ((!playerSwing[0] || !playerSwing[1]) && (actualResult[activePlayer] > 21)) determineWinner(actualResult);
+                }
+            } else if (actualResult[activePlayer] === 21) {
+                alert(`Napewno kolejna karta :-)?? Masz już oczko i ${actualResult[activePlayer]} punktów :-) Spasuj`)
             }
         }
     }
 
     //take point function
     function takePoints() {
-        if (actualResult === 0) {
-            startGame();
-        } else if (actualResult > 0 && actualResult < 21) {
-            notWins++;
-            allResults.textContent = notWins;
-            startGame();
-        } else if (actualResult === 21) {
-            gamePlaying = false;
-            twentyOnesResults++;
-            twentyOnes.textContent = twentyOnesResults;
-            congratulationsPopup.style.display = 'flex'
-        } else if (actualResult === 22 && cardCounter === 2) {
-            gamePlaying = false;
-            twoAcesResults++;
-            twoAces.textContent = twoAcesResults;
-            twoAcesPopup.style.display = 'flex';
-        } else {
-            gamePlaying = false;
-            youLostPopup.style.display = 'flex';
+        playerSwing[activePlayer] = false;
+        activePlayer++;
+        if (activePlayer === 2) activePlayer = 0;
+        if (!playerSwing[0] && !playerSwing[1]) {
+            determineWinner(actualResult);
         }
     }
 
-    //percentage show
-    function showPercentages () {
-        if (gameCounter > 0) {
-            document.querySelector('#notwins').textContent = `${((notWins / gameCounter) * 100).toFixed(2)}%`;
-            document.querySelector('#twentyones').textContent = `${((twentyOnesResults / gameCounter) * 100).toFixed(2)}%`;
-            document.querySelector('#twoaces').textContent = `${((twoAcesResults / gameCounter) * 100).toFixed(2)}%`;
-            document.querySelector('#lost').textContent = `${((gameLostResults / gameCounter) * 100).toFixed(2)}%`;
+    //winner determining
+    function determineWinner(array) {
+        if (array[0] < 21 && array[1] < 21) {
+            if (array[0] > array[1]) {
+                wins[0]++;
+                wins0.textContent = wins[0];
+                gameLostResults[1]++;
+                lost1.textContent = gameLostResults[1];
+                document.querySelector('.who-wins p').textContent = 'GRACZ 1 WYGRYWA!!';
+                whoWinsPopup.style.display = 'flex';
+            } else if (array[0] < array[1]) {
+                wins[1]++;
+                wins1.textContent = wins[1];
+                gameLostResults[0]++;
+                lost0.textContent = gameLostResults[0];
+                document.querySelector('.who-wins p').textContent = 'GRACZ 2 WYGRYWA!!';
+                whoWinsPopup.style.display = 'flex';
+            } else {
+                drawsCounter++;
+                document.querySelector('#draw').textContent = drawsCounter;
+                document.querySelector('.who-wins p').textContent = 'REMIS!!!!';
+                whoWinsPopup.style.display = 'flex';
+            }
+        } else if (((array[0] === 22 || array[1] === 22) && cardCounter === 4) || (array[0] === 22 && array[1] === 22 
+            && cardCounter === 4)) {
+            if(array[0] ===22 && array[1] === 22) {
+                drawsCounter++;
+                document.querySelector('#draw').textContent = drawsCounter;
+                document.querySelector('.congratulations_two_aces p').textContent = 'NIEBYWAŁY REMIS!!JAK SZÓSTKA W LOTTO!!DWA PERSKIE OCZKA!';
+                twoAcesPopup.style.display = 'flex';
+            } else if (array[0] === 22) {
+                twoAcesResults[0]++;
+                twoAces0.textContent = twoAcesResults[0];
+                gameLostResults[1]++;
+                lost1.textContent = gameLostResults[1];
+                document.querySelector('.congratulations_two_aces p').textContent = ' PERSKIE OCZKO DLA GRACZA 1, KTÓRY WYGRYWA!!';
+                twoAcesPopup.style.display = 'flex';
+            } else if (array[1] === 22) {
+                twoAcesResults[1]++;
+                twoAces1.textContent = twoAcesResults[1];
+                gameLostResults[0]++;
+                lost1.textContent = gameLostResults[0];
+                document.querySelector('.congratulations_two_aces p').textContent = ' PERSKIE OCZKO DLA GRACZA 2, KTÓRY WYGRYWA!!';
+                twoAcesPopup.style.display = 'flex';
+            }
+        } else if ((array[0] === 21 || array[1] === 21) || (array[0] === 21 && array[1] === 21)){
+            if(array[0] ===21 && array[1] === 21) {
+                drawsCounter++;
+                document.querySelector('#draw').textContent = drawsCounter;
+                document.querySelector('.who-wins p').textContent = 'ALE REMIS!!!!DWA OCZKA';
+                whoWinsPopup.style.display = 'flex';
+            } else if (array[0] === 21) {
+                twentyOnesResults[0]++;
+                twentyOne0.textContent = twentyOnesResults[0];
+                gameLostResults[1]++;
+                lost1.textContent = gameLostResults[1];
+                document.querySelector('.who-wins p').textContent = 'OCZKO DLA GRACZA 1, KTÓRY WYGRYWA!!';
+                whoWinsPopup.style.display = 'flex';
+            } else if (array[1] === 21) {
+                twentyOnesResults[1]++;
+                twentyOne1.textContent = twentyOnesResults[1];
+                gameLostResults[0]++;
+                lost1.textContent = gameLostResults[0];
+                document.querySelector('.who-wins p').textContent = 'OCZKO DLA GRACZA 2, KTÓRY WYGRYWA!!';
+                whoWinsPopup.style.display = 'flex';
+            }
+        } else if (array[0] >= 22 && cardCounter > 4) {
+            wins[1]++;
+            wins1.textContent = wins[1];
+            gameLostResults[0]++;
+            lost0.textContent = gameLostResults[0];
+            document.querySelector('.who-wins p').textContent = 'GRACZ 2 WYGRYWA!!';
+            whoWinsPopup.style.display = 'flex';
+        } else if (array[1] >= 22 && cardCounter > 4){
+            wins[0]++;
+            wins0.textContent = wins[0];
+            gameLostResults[1]++;
+            lost1.textContent = gameLostResults[1];
+            document.querySelector('.who-wins p').textContent = 'GRACZ 1 WYGRYWA!!';
+            whoWinsPopup.style.display = 'flex';
         }
+        gameCounter++;
+        document.querySelector('#game_counter').textContent = gameCounter;
     }
 
-    //event listeners
+    //buttons event listeners
     document.querySelectorAll('.start').forEach(btn => btn.addEventListener('click', startGame));
     document.querySelector('.take_card').addEventListener('click', takeCard);
     document.querySelector('.stop').addEventListener('click', takePoints);
